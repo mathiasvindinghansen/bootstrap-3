4b5732410a36ae7ea69ceb4a15a6fe9f21bf2596
mathiasvindinghansen <mvh@the-lead.dk>
2025-11-03 11:38:24 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 function purchaseProduct() {
     console.log('u bought something');
 }
+
+$(function() {
+    for ($i = 0; $i < 10; $i++) {
+        $.get('ajax/product.html', function(data) {
+            $(data).appendTo('.product-list')
+        });
+
+        $.get('ajax/list-item.html', function(data) {
+            $(data).appendTo('.product-search .list')
+        });
+    }
+});
