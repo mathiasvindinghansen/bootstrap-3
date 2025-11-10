7d7470adccf939191e9c9826120dd8e1ef64a508
mathiasvindinghansen <mvh@the-lead.dk>
2025-11-10 14:04:23 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 function purchaseProduct() {
-    console.log('u bought something');
+    console.log("bought");
+}
+
+function removeProduct(productElement) {
+    $(productElement).parent().parent().remove();
 }
 
 $(function() {
     for ($i = 0; $i < 10; $i++) {
         $.get('ajax/product.html', function(data) {
-            $(data).appendTo('.product-list')
+            $(data).appendTo('.product-list');
         });
 
         $.get('ajax/list-item.html', function(data) {
-            $(data).appendTo('.product-search .list')
+            $(data).appendTo('.product-search .list');
+        });
+
+        $.get('ajax/cart-item.html', function(data) {
+            $(data).appendTo('.cart-items');
         });
     }
+
+    $('#cart a').on('click', function() {
+        $('#cart .cart').toggleClass('hidden');
+    }).children().on('click', function() { //ignore children of cart
+        return false;
+    });
 });
